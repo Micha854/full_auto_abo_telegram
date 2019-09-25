a59ba98b10f31acfe481372a0f1f4334bb0cd32f
Micha854 <49801772+Micha854@users.noreply.github.com>
2019-09-25 16:40:39 +0200

    Update index.php
    sort ASC

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include_once("config.php");
 
-$query = "SELECT * FROM products ORDER BY id DESC";
+$query = "SELECT * FROM products ORDER BY id ASC";
 $result = $mysqli->query($query);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
