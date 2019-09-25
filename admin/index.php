7dc046476e49723b2629fe99ffc5e936e4656b64
Micha854 <49801772+Micha854@users.noreply.github.com>
2019-09-26 01:32:02 +0200

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-<title>Pogo L&uuml;beck - ADMIN</title>
+<title><?=$WebsiteTitle ?> - ADMIN</title>
 <style type="text/css">
 table {
    border-collapse: collapse; 
