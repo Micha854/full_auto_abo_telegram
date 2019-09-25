7102178931c97cf025cdedc2b47ec647631b7631
Micha854 <49801772+Micha854@users.noreply.github.com>
2019-09-26 01:30:48 +0200

    Update _edit_user.php

--- admin/_edit_user.php
+++ admin/_edit_user.php
@@ -22,31 +22,40 @@ $row = $result->fetch_array(MYSQLI_ASSOC);
 
 
 if($_POST["submit"] and $_POST["user"]) {
+	
+	$InputUser = $row["TelegramUser"]; // old Username delete
+	
+	//$htpasswd = new Htpasswd('.htpasswd');
+	//$htpasswd->deleteUser($InputUser);
+
+	$newUser = mysqli_real_escape_string($mysqli, $_POST["user"]);
+	$newAdd = $_POST["user"];
+	
+	$htpasswd = new Htpasswd('.htpasswd');
+	$htpasswd->deleteUser($InputUser);
+	$htpasswd->addUser($newAdd, $passwd);
+					
+	mysqli_query($mysqli, "UPDATE ".$tbl." SET TelegramUser = '".$newUser."', pass = '".$passwd."' WHERE id = ".$row["id"]);
+	
 	echo '<div style="display:none">';
 	include 'madeline.php';
 
 	$MadelineProto = new \danog\MadelineProto\API('session.madeline');
 	$MadelineProto->start();
 
-	$InputUser = $row["TelegramUser"]; // old Username delete
+	$InputChannel = 'https://t.me/joinchat/AAAAAEOzl0uIG6rC2xuqjQ'; // chanel
+
+	//echo "delete ".$row["TelegramUser"]."<br>";
+	
 	$ChatBannedRights = ['_' => 'chatBannedRights', 'view_messages' => true, 'until_date' => 0];
 
 	$Updates = $MadelineProto->channels->editBanned(['channel' => $InputChannel, 'user_id' => $InputUser, 'banned_rights' => $ChatBannedRights, ]);
 	
-	//$htpasswd = new Htpasswd('.htpasswd_user');
-	//$htpasswd->deleteUser($InputUser);
-
-	$newUser = mysqli_real_escape_string($mysqli, $_POST["user"]);
-	$newAdd = $_POST["user"];
 	
-	//$htpasswd = new Htpasswd('.htpasswd_user');
-	//$htpasswd->addUser($newAdd, $passwd);
-					
-	mysqli_query($mysqli, "UPDATE ".$tbl." SET TelegramUser = '".$newUser."', pass = '".$passwd."' WHERE id = ".$row["id"]);
 	
 	$Updates = $MadelineProto->channels->inviteToChannel(['channel' => $InputChannel, 'users' => [$newAdd, $newAdd], ]);
 	echo '</div>';
-	echo '<h3 style="background:#009900; color:#FFFFFF; padding:5px; text-align:center"><a href="admin/">back</a> | user has been changed to '.$newAdd.'</h3>';
+	echo '<h3 style="background:#009900; color:#FFFFFF; padding:5px; text-align:center"><a href="/tme/admin/">back</a> | user has been changed to '.$newAdd.'</h3>';
 }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
@@ -54,7 +63,7 @@ if($_POST["submit"] and $_POST["user"]) {
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-<title>Pogo L&uuml;beck - ADMIN EDIT</title>
+<title><?=$WebsiteTitle ?> - ADMIN EDIT</title>
 <style type="text/css">
 html {
 font-size:20px
