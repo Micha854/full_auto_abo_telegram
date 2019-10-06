52819776294178b350e6c31ed581c660ea91ff91
Micha854 <49801772+Micha854@users.noreply.github.com>
2019-10-06 12:11:30 +0200

    Update _edit_user.php

--- admin/_edit_user.php
+++ admin/_edit_user.php
@@ -7,7 +7,8 @@ if ($mysqli->connect_error) {
 }
 
 function generateRandomString($length = 10) {
-	return substr(str_shuffle(str_repeat(implode('', range('!','z')), $length)), 0, $length);
+	//return substr(str_shuffle(str_repeat(implode('', range('!','z')), $length)), 0, $length);
+	return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
 }
 $passwd = generateRandomString(8);
 
