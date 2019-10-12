cb8fb40cf94a8932af38477384399146c13d5b06
Micha854 <49801772+Micha854@users.noreply.github.com>
2019-10-12 13:34:02 +0200

    delete on all chanels

--- admin/_delete_user.php
+++ admin/_delete_user.php
@@ -8,16 +8,18 @@ include 'Htpasswd.php';
 $MadelineProto = new \danog\MadelineProto\API('session.madeline');
 $MadelineProto->start();
 
-while($row = $result->fetch_array())
-{
-echo "delete ".$row["TelegramUser"]."<br>";
-$InputUser = $row["TelegramUser"]; // username
-$ChatBannedRights = ['_' => 'chatBannedRights', 'view_messages' => true, 'until_date' => 0];
+while($row = $result->fetch_array()) {
+	echo "delete ".$row["TelegramUser"]."<br>";
+	$InputUser = $row["TelegramUser"]; // username
+	$ChatBannedRights = ['_' => 'chatBannedRights', 'view_messages' => true, 'until_date' => 0];
 
-$Updates = $MadelineProto->channels->editBanned(['silent' => false, 'channel' => $InputChannel, 'user_id' => $InputUser, 'banned_rights' => $ChatBannedRights, ]);
-mysqli_query($mysqli, "DELETE FROM ".$tbl." WHERE id = ".$row["id"]." ");
+	foreach ( $mysqli->query("SELECT url FROM channels WHERE id IN (".$row["channels"].")") as $channel ) {
+		$Updates = $MadelineProto->channels->editBanned(['silent' => false, 'channel' => $channel["url"], 'user_id' => $InputUser, 'banned_rights' => $ChatBannedRights, ]);
+	}
 
-$htpasswd = new Htpasswd('.htpasswd_user');
-$htpasswd->deleteUser($InputUser);
+	mysqli_query($mysqli, "DELETE FROM ".$tbl." WHERE id = ".$row["id"]." ");
+
+	$htpasswd = new Htpasswd('.htpasswd');
+	$htpasswd->deleteUser($InputUser);
 }
 ?>
