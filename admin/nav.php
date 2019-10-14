5bbaf9acfab142dde085e47ee35d3fac0e44100f
pixl <41138223+nepixl@users.noreply.github.com>
2019-10-14 22:18:54 +0200

    added bootstrap support + better overview - first shot

---
+++ admin/nav.php
@@ -0,0 +1,25 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="/admin">Adminpanel</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Benutzercenter
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="/admin">Übersicht</a>
+          <a class="dropdown-item" href="_newUser.php">Hinzufügen</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="/admin">Reset</a>
+        </div>
+      </li>
+    </ul>
+        <form class="form-inline mt-2 mt-md-0">
+          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+  </div>
+</nav>
