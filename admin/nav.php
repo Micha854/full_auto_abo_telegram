82871f6cc1b3bba8bdfbb503c5e7c79dee1010a0
pixl <41138223+nepixl@users.noreply.github.com>
2019-10-14 22:26:35 +0200

    recolor navbar

--- admin/nav.php
+++ admin/nav.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="/admin">Adminpanel</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -18,8 +18,8 @@
       </li>
     </ul>
         <form class="form-inline mt-2 mt-md-0">
-          <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <input class="form-control mr-sm-2" type="text" placeholder="Suche" aria-label="Search">
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Suche</button>
         </form>
   </div>
 </nav>
