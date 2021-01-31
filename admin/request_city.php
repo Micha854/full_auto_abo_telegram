ab07391ca2616631cc68fa5ee9a077a2d98440c5
Micha <ms@starburger.de>
2021-01-31 23:27:25 +0100

    select table from config

--- admin/request_city.php
+++ admin/request_city.php
@@ -2,7 +2,7 @@
 require_once dirname(__FILE__) . '/../config.php';
 
 if(!empty($_POST["keyword"])) {
-    $query = "SELECT city FROM `abos` WHERE city like '" . $_POST["keyword"] . "%' ORDER BY city";
+    $query = "SELECT city FROM ".$tbl." WHERE city like '" . $_POST["keyword"] . "%' ORDER BY city";
     $result = $mysqli->query($query);
     if(!empty($result)) { ?>
         <ul id="country-list"><?php
